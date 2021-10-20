61fbf3abeb156ccd3969ee964dcb8a08bc9b375f
Christian Dean Demesa <christiandeandemesa@gmail.com>
2021-10-20 08:54:44 -0700

    10/20/21 commit

---
+++ Kata6.js
@@ -0,0 +1,27 @@
+/* Given: an array containing hashes of names
+
+Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand. */
+
+function list(names){
+    let str = '';
+    if (names.length !== 0){
+        let last = names.pop();
+        str = names.map((obj, i, arr) => {
+            if (i !== arr[arr.length - 1]){
+                console.log(obj);
+                console.log(i);
+                console.log(arr[arr.length - 1]);
+                console.log(obj.name);
+                return obj.name;
+            }
+        }).join(', ')
+        str += str !== '' ? ' & ' + last.name : last.name;
+    }
+    return str;
+}
+
+console.log(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'},{name: 'Homer'},{name: 'Marge'}]))
+console.log(list([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}]))
+console.log(list([{name: 'Bart'},{name: 'Lisa'}]))
+console.log(list([{name: 'Bart'}]))
+console.log(list([]))
