1629ea2d4e8c279a7d8aec790db898998f9bf781
Christian Dean Demesa <christiandeandemesa@gmail.com>
2021-11-06 20:04:25 -0700

    11/6/21

---
+++ Rot13.js
@@ -0,0 +1,30 @@
+/*
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. 
+ROT13 is an example of the Caesar cipher.
+
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special 
+characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet 
+should be shifted, like in the original Rot13 "implementation".
+*/
+
+function rot13(message) {
+
+    // Created a const to hold every uppercase and lowercase letter in the alphabet in order, and a repeat of the first thirteen letters. 
+    const alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+
+    /*
+    /[a-z]/gi is a use of Regex. [a-z] is a character set representing a range from letter a to letter z, g indicating a global search, 
+    and i indicating a case insensitive search.
+
+    .indexOf() returns the position of the first occurence of the value in the string.
+
+    Used an arrow function to assign letter the value of what alphabet is.
+
+    .replace() will replace the first parameter (e.g. /[a-z]/gi) with the second parameter (e.g. letter).
+    */
+    return message.replace(/[a-z]/gi, letter => alphabet[alphabet.indexOf(letter) + 13]);
+}
+
+console.log(rot13('grfg')); // test
+console.log(rot13('Grfg')); // Test
+console.log(rot13('V nz 25 lrnef byq.')) // I am 25 years old.
