031fdbd601b38ff3172069c64c5c0a566b6d3cbd
Christian Dean Demesa <christiandeandemesa@gmail.com>
2022-01-18 18:57:31 -0800

    1/18/22 commit

---
+++ HackerRank Algorithms/30 Days of Code/Day 0 Hello World.java
@@ -0,0 +1,8 @@
+/*
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally 
+print the value of your variable on a second line.
+*/
+
+// Prints both strings one after another in the console. inputString is provided by HackerRank.
+print.out.systemln("Hello, World.");
+print.out.systemln(inputString);
